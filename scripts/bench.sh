198dd691f8fa426bb0b5a53d695a088d20e284eb
Aditya Chandak <adityachandak287@gmail.com>
2024-04-23 23:42:13 +0530

    feat: build typescript files, add node to comparison

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+npm run build
+
 for measurements_file in $(ls -r measurements*.txt)
 do
   echo "Running benchmarks for $measurements_file"
@@ -10,7 +12,9 @@ do
     --min-runs 1 \
     --warmup 1 \
     --parameter-list input_file $measurements_file \
+    --export-markdown comparison_$measurements_file.md \
     -n 'python bruteforce {input_file}' 'python python/brute_force.py {input_file}' \
-    -n 'bun bruteforce {input_file}' 'bun run node/brute-force.ts {input_file}'
+    -n 'bun bruteforce {input_file}' 'bun run node/brute-force.ts {input_file}' \
+    -n 'node bruteforce {input_file}' 'node node/dist/brute-force.js {input_file}'
 
 done
