009015fc71d692a4252c131fdd3bfc0ce4df9a29
Aditya Chandak <adityachandak287@gmail.com>
2024-05-02 21:09:30 +0530

    chore: update bench script to include v1 implementations

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -15,6 +15,9 @@ do
     --export-markdown comparison_$measurements_file.md \
     -n 'python bruteforce {input_file}' 'python python/brute_force.py {input_file}' \
     -n 'bun bruteforce {input_file}' 'bun run node/brute-force.ts {input_file}' \
-    -n 'node bruteforce {input_file}' 'node node/dist/brute-force.js {input_file}'
+    -n 'node bruteforce {input_file}' 'node node/dist/brute-force.js {input_file}' \
+    -n 'bun v1 {input_file}' 'bun run node/v1.ts {input_file}' \
+    -n 'python v1 {input_file}' 'python python/v1.py {input_file}' \
+    -n 'node v1 {input_file}' 'node node/dist/v1.js {input_file}'
 
 done
