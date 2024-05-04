406c156f949644aa9bfa2bb56ef6a7f5cda42955
Aditya Chandak <adityachandak287@gmail.com>
2024-05-04 15:01:26 +0530

    feat(go): add cpu profile and tracing flags

--- go/main.go
+++ go/main.go
@@ -3,16 +3,49 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"runtime/pprof"
+	"runtime/trace"
 )
 
 func main() {
 	inputFile := flag.String("file", "", "Measurements input file")
 	impl := flag.String("impl", "brute_force", "Implementation to use [brute_force]")
+	cpuprofile := flag.String("cpuprofile", "", "Write cpu profile to file")
+	tracefile := flag.String("trace", "", "Write traces to file")
 
 	flag.Parse()
 
 	if *inputFile == "" {
 		log.Panic("Measurements input file requred!")
+	}
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		err = pprof.StartCPUProfile(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	if *tracefile != "" {
+		f, err := os.Create(*tracefile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		err = trace.Start(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer trace.Stop()
 	}
 
 	switch *impl {
